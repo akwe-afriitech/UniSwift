43562c3768c23f97aa1fc75d3644fba62d5ea003
Earl Millen Toh Akwe <69284908+akwe-afriitech@users.noreply.github.com>
2022-09-27 00:14:59 +0100

    Add files via upload

---
+++ lib/rests/constants.dart
@@ -0,0 +1,14 @@
+
+
+class ApiConstants {
+  static String baseUrl = 'https://jsonplaceholder.typicode.com';
+  static String usersEndpoint = '/users';
+}
+
+
+class Model{
+  final data = JSON[0]['id'];
+
+  static get JSON => null;
+
+}
